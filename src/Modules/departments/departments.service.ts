b4a2ca5daa9d1638b854c2210bfe47babacc4471
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2025-02-09 20:15:37 +0300

    department services

--- src/Modules/departments/departments.service.ts
+++ src/Modules/departments/departments.service.ts
@@ -1,26 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Department } from './entities/department.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DepartmentsService {
-  create(createDepartmentDto: CreateDepartmentDto) {
-    return 'This action adds a new department';
+  constructor(
+    @InjectRepository(Department)
+    private readonly departmentRepository: Repository<Department>,
+  ) {}
+  async create(createDepartmentDto: CreateDepartmentDto): Promise<Department> {
+    const department = this.departmentRepository.create(createDepartmentDto);
+    return this.departmentRepository.save(department);
   }
 
-  findAll() {
-    return `This action returns all departments`;
+  async findAll(): Promise<Department[]> {
+    return this.departmentRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} department`;
+  async findOne(id: number): Promise<Department[]> {
+    const department = this.departmentRepository.find({
+      where: { departmentId: id },
+      relations: ['employees'],
+    });
+    return department;
   }
 
-  update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
-    return `This action updates a #${id} department`;
+  async update(
+    id: number,
+    updateDepartmentDto: UpdateDepartmentDto,
+  ): Promise<Department> {
+    const department = await this.departmentRepository.preload({
+      departmentId: id,
+      ...updateDepartmentDto,
+    });
+    if (!department) {
+      throw new Error(`Department with ID ${id} not found`);
+    }
+    return this.departmentRepository.save(department);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} department`;
+  async remove(id: number): Promise<void> {
+    const department = await this.departmentRepository.delete(id);
+    if (department.affected === 0) {
+      throw new Error(`Department with ID ${id} not found`);
+    }
   }
 }

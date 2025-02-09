c440b5912656ff32643a80a5fefa26bafe55082f
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2025-02-09 20:33:53 +0300

    evaluation services

--- src/Modules/evaluations/evaluations.service.ts
+++ src/Modules/evaluations/evaluations.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEvaluationDto } from './dto/create-evaluation.dto';
 import { UpdateEvaluationDto } from './dto/update-evaluation.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Evaluation } from './entities/evaluation.entity';
 
 @Injectable()
 export class EvaluationsService {
-  create(createEvaluationDto: CreateEvaluationDto) {
-    return 'This action adds a new evaluation';
+  constructor(
+    @InjectRepository(Evaluation)
+    private readonly evaluationRepository: Repository<Evaluation>,
+  ) {}
+  async create(createEvaluationDto: CreateEvaluationDto): Promise<Evaluation> {
+    const evaluation =
+      await this.evaluationRepository.create(createEvaluationDto);
+    return await this.evaluationRepository.save(evaluation);
   }
 
-  findAll() {
-    return `This action returns all evaluations`;
+  async findAll(): Promise<Evaluation[]> {
+    return this.evaluationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} evaluation`;
+  async findOne(id: number): Promise<Evaluation> {
+    const evaluation = await this.evaluationRepository.findOne({
+      where: { evaluationId: id },
+      relations: ['employee'],
+    });
+    return evaluation;
   }
 
-  update(id: number, updateEvaluationDto: UpdateEvaluationDto) {
-    return `This action updates a #${id} evaluation`;
+  async update(
+    id: number,
+    updateEvaluationDto: UpdateEvaluationDto,
+  ): Promise<Evaluation> {
+    const evaluation = await this.evaluationRepository.preload({
+      evaluationId: id,
+      ...updateEvaluationDto,
+    });
+    return this.evaluationRepository.save(evaluation);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} evaluation`;
+  async remove(id: number): Promise<void> {
+    const result = await this.evaluationRepository.delete(id);
+    if (result.affected === 0) {
+      throw new Error(`Evaluation with ID ${id} not found`);
+    }
   }
 }

37b9471bf30f2fefa8d58cbd732fac67d59a30ce
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2025-02-10 09:55:52 +0300

    salaries services

--- src/Modules/salaries/salaries.service.ts
+++ src/Modules/salaries/salaries.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSalaryDto } from './dto/create-salary.dto';
 import { UpdateSalaryDto } from './dto/update-salary.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Salary } from './entities/salary.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SalariesService {
-  create(createSalaryDto: CreateSalaryDto) {
-    return 'This action adds a new salary';
+  constructor(
+    @InjectRepository(Salary)
+    private readonly salaryRepositroy: Repository<Salary>,
+  ) {}
+  async create(createSalaryDto: CreateSalaryDto): Promise<Salary> {
+    const salaries = this.salaryRepositroy.create(createSalaryDto);
+    return this.salaryRepositroy.save(salaries);
   }
 
-  findAll() {
-    return `This action returns all salaries`;
+  async findAll(): Promise<Salary[]> {
+    const salaries = await this.salaryRepositroy.find({
+      relations: ['employees'],
+    });
+    return salaries;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} salary`;
+  async findOne(id: number): Promise<Salary> {
+    const salaries = await this.salaryRepositroy.findOne({
+      where: { salaryId: id },
+      relations: ['employees'],
+    });
+    return salaries;
   }
 
-  update(id: number, updateSalaryDto: UpdateSalaryDto) {
-    return `This action updates a #${id} salary`;
+  async update(id: number, updateSalaryDto: UpdateSalaryDto): Promise<Salary> {
+    const salary = await this.salaryRepositroy.preload({
+      salaryId: id,
+      ...updateSalaryDto,
+    });
+    return this.salaryRepositroy.save(salary);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} salary`;
+  async remove(id: number): Promise<void> {
+    const result = await this.salaryRepositroy.delete(id);
+    if (result.affected === 0) {
+      throw new Error(`Salary with ID ${id} not found`);
+    }
   }
 }
